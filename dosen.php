52213c39d7db148d9e7dd7297855c4e4e2cc33c5
ilham <ilhamdwiischan@gmail.com>
2026-03-13 13:17:26 +0700

    add feature dosen in website

---
+++ dosen.php
@@ -0,0 +1,237 @@
+<?php include 'partials/header.php'; ?>
+
+<main class="main">
+
+        <!-- Page Title -->
+        <div class="page-title" data-aos="fade">
+            <nav class="breadcrumbs">
+                <div class="container">
+                    <ol>
+                        <li><a href="index.html">Home</a></li>
+                        <li class="current">Dosen</li>
+                    </ol>
+                </div>
+            </nav>
+            <div class="heading">
+                <div class="container">
+                    <div class="row d-flex justify-content-center text-center">
+                        <div class="col-lg-8">
+                            <h1>Dosen</h1>
+                            <p class="mb-0">Belajar langsung dari para dosen dan praktisi berpengalaman di bidang Farmasi.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- End Page Title -->
+
+        <!-- Team Section -->
+        <section id="team" class="team section">
+            <div class="container">
+                <div class="row gy-4">
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+                        <div class="team-member">
+                            <div class="member-img">
+                                <img src="assets/img/team/farmasi/Aulia-Asmawati.png" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>Aulia Asmawati, S.Farm., M.M.</h4>
+                                <span>Dosen Tetap</span>
+                                <p class="text-start" style="font-size: 0.9em;">
+                                    <strong>S1:</strong> Universitas Gadjah Mada (S.Farm.)<br>
+                                    <strong>S2:</strong> Universitas Mercu Buana (M.M.)
+                                </p>
+                            </div>
+                        </div>
+                    </div><!-- End Team Member -->
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+                        <div class="team-member">
+                            <div class="member-img">
+                                <img src="assets/img/team/farmasi/BU-AYU.png" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>apt. Ayu Izzatin Haifa, S.Farm., M.B.A.</h4>
+                                <span>Dosen Tetap</span>
+                                <p class="text-start" style="font-size: 0.9em;">
+                                    <strong>S1:</strong> Universitas Padjadjaran (Farmasi)<br>
+                                    <strong>S2:</strong> Institut Teknologi Bandung (M.B.A.)
+                                </p>
+                            </div>
+                        </div>
+                    </div><!-- End Team Member -->
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+                        <div class="team-member">
+                            <div class="member-img">
+                                <img src="assets/img/team/farmasi/Bu-Elsa.png" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>Elsa Marliana, S.Farm., M.Farm.</h4>
+                                <span>Dosen Tetap</span>
+                                <p class="text-start" style="font-size: 0.9em;">
+                                    <strong>S1:</strong> Universitas Garut (Farmasi)<br>
+                                    <strong>S2:</strong> Universitas Pancasila (Ilmu Kefarmasian)
+                                </p>
+                            </div>
+                        </div>
+                    </div><!-- End Team Member -->
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="400">
+                        <div class="team-member">
+                            <div class="member-img">
+                                <img src="assets/img/team/farmasi/Farah-Sulistyaningtyas.png" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>Farah Sulistyaningtyas, S.Farm., M.Farm.</h4>
+                                <span>Asisten Ahli / Dosen Tetap</span>
+                                <p class="text-start" style="font-size: 0.9em;">
+                                    <strong>S1:</strong> Universitas Ahmad Dahlan (Farmasi)<br>
+                                    <strong>S2:</strong> Universitas Pancasila (Ilmu Kefarmasian)
+                                </p>
+                            </div>
+                        </div>
+                    </div><!-- End Team Member -->
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="500">
+                        <div class="team-member">
+                            <div class="member-img">
+                                <img src="assets/img/team/farmasi/Fathia-Michella-Taurisandy-Putri.png" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>Fathia Michella T. P., S.Farm., M.S.Farm.</h4>
+                                <span>Dosen Tetap</span>
+                                <p class="text-start" style="font-size: 0.9em;">
+                                    <strong>D3:</strong> Akademi Farmasi Yarsi Pontianak (Farmasi)<br>
+                                    <strong>S1:</strong> Universitas Garut (Farmasi)<br>
+                                    <strong>S2:</strong> Sekolah Farmasi Institut Teknologi Bandung (Farmakologi – Toksikologi)
+                                </p>
+                            </div>
+                        </div>
+                    </div><!-- End Team Member -->
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="600">
+                        <div class="team-member">
+                            <div class="member-img">
+                                <img src="assets/img/team/farmasi/Fauzia-Azzahra.jpg" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>apt. Fauzia Azzahra, M.S.Farm.</h4>
+                                <span>Dosen Tetap</span>
+                                <p class="text-start" style="font-size: 0.9em;">
+                                    <strong>S1:</strong> Universitas Airlangga (S.Farm)<br>
+                                    <strong>Profesi:</strong> Universitas Airlangga (Apt)<br>
+                                    <strong>S2:</strong> Institut Teknologi Bandung (M.S.Farm)
+                                </p>
+                            </div>
+                        </div>
+                    </div><!-- End Team Member -->
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="700">
+                        <div class="team-member">
+                            <div class="member-img">
+                                <img src="assets/img/team/farmasi/Nindya-Sekar-Mayuri.png" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>Nindya Sekar Mayuri, S.Pd., M.Si.</h4>
+                                <span>Dosen Tetap</span>
+                                <p class="text-start" style="font-size: 0.9em;">
+                                    <strong>S1:</strong> Universitas Pendidikan Indonesia (S.Pd.)<br>
+                                    <strong>S2:</strong> Institut Teknologi Bandung (M.Si.)
+                                </p>
+                            </div>
+                        </div>
+                    </div><!-- End Team Member -->
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="800">
+                        <div class="team-member">
+                            <div class="member-img">
+                                <img src="assets/img/team/tisa-amalia.png" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>Tisa Amalia, S.SiT., M.H(Kes)</h4>
+                                <span>Dosen Tetap</span>
+                                <p class="text-start" style="font-size: 0.9em;">
+                                    <strong>D IV:</strong> Ngudi Waluyo Unggaran (Kebidanan Pendidik)<br>
+                                    <strong>S2:</strong> Universitas Swadaya Gunung Jati (Hukum Kesehatan)
+                                </p>
+                            </div>
+                        </div>
+                    </div><!-- End Team Member -->
+
+                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="900">
+                        <div class="team-member">
+                            <div class="member-img">
+                                <img src="assets/img/team/farmasi/Vina-Fauziah.jpg" class="img-fluid" alt="">
+                                <div class="social">
+                                    <a href=""><i class="bi bi-twitter-x"></i></a>
+                                    <a href=""><i class="bi bi-facebook"></i></a>
+                                    <a href=""><i class="bi bi-instagram"></i></a>
+                                    <a href=""><i class="bi bi-linkedin"></i></a>
+                                </div>
+                            </div>
+                            <div class="member-info">
+                                <h4>apt. Vina Fauziah, M.S.Farm.</h4>
+                                <span>Dosen Tetap</span>
+                                <p class="text-start" style="font-size: 0.9em;">
+                                    <strong>S1:</strong> Universitas Padjadjaran (Farmasi)<br>
+                                    <strong>Profesi:</strong> Universitas Padjadjaran (Apt)<br>
+                                    <strong>S2:</strong> Institut Teknologi Bandung (M.S.Farm)
+                                </p>
+                            </div>
+                        </div>
+                    </div><!-- End Team Member -->
+                </div>
+            </div>
+        </section><!-- /Team Section -->
+
+    </main>
+
+<?php include 'partials/footer.php'; ?>
